742b72ef9bb128aca5c85fff705d1ec77f29e201
Jose Constela <jose@joseconstela.com>
2020-03-01 12:18:41 +0100

    Add debuging info

--- imports/modules/install/server/methods.js
+++ imports/modules/install/server/methods.js
@@ -9,8 +9,12 @@ import { ROLES } from '../../_common/both/teams'
 
 import { check } from 'meteor/check'
 
+const debug = require('debug')('tideflow:installation:core')
+
 Meteor.methods({
   'install-platform' (details) {
+    debug('Method execution')
+
     check(details, {
       siteName: String,
       userName: String,
@@ -18,8 +22,12 @@ Meteor.methods({
       password: String
     })
 
+    debug('Finding settings...')
+
     let one = Settings.findOne()
     if (one) return new Meteor.Error('not-allowed')
+
+    debug('Creating user...')
 
     // Create user
     const userInfo = {
@@ -29,19 +37,23 @@ Meteor.methods({
     }
     const userId = Accounts.createUser(userInfo)
 
-    Roles.createRole(ROLES.SUPER, {unlessExists: true})
-    Roles.addUsersToRoles(userId, ROLES.SUPER)
+    debug('Creating roles...')
 
     Object.keys(ROLES).map(r => {
-      Roles.createRole(ROLES[r])
+      Roles.createRole(ROLES[r], {unlessExists: true})
     })
+    Roles.addUsersToRoles(userId, ROLES.SUPER)
+
+    debug('Creating team...')
 
     createUsersTeam(Object.assign(userInfo, {_id: userId}))
     
     // Verify email automatically
+    debug('Verifying user email...')
     Meteor.users.update({_id:userId}, {$set:{'emails.0.verified': true}})
 
     // Store settings
+    debug('Update site settings...')
     Settings.update({
       public: true,
       type: 'siteSettings'
