6ae7b171c55d7e548a353feebd1dc4a5b085ad40
Jose Constela <jose@joseconstela.com>
2020-04-25 21:23:17 +0200

    Fix missing url param

--- imports/modules/services/client/ui/index/events.js
+++ imports/modules/services/client/ui/index/events.js
@@ -4,6 +4,7 @@ import { Router } from 'meteor/iron:router'
 Template.servicesIndexTableService.events({
   'click': (event, template) => {
     Router.go('services.one.edit', {
+      teamId: Router.current().params.teamId,
       _id: template.data._id,
       type: template.data.type
     })
