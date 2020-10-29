56952406209353955839820234e7989055fb0b3a
Jose Constela <jose@joseconstela.com>
2020-10-29 22:43:59 +0100

    Postman collection download for endpoints

--- imports/services/endpoint/client/events.js
+++ imports/services/endpoint/client/events.js
@@ -1,16 +1,39 @@
 import { Template } from 'meteor/templating'
 
+const slugify = require('slugify')
 const uuidv4 = require('uuid/v4')
+
+const download = (text, name, type) => {
+  var file = new Blob([text], {type: type});
+  var isIE = /*@cc_on!@*/false || !!document.documentMode;
+  if (isIE)
+  {
+      window.navigator.msSaveOrOpenBlob(file, name);
+  }
+  else
+  {
+      var a = document.createElement('a');
+      a.href = URL.createObjectURL(file);
+      a.download = name;
+      a.click();
+  }
+}
+
+Template.downloadPostman.events({
+  'click': (event, template) => {
+    const d = template.data
+    const path = $(d['subfix-val']).val()
+    const url = `${d['prefix-text']}${d.text}${path}`
+    Meteor.call('s-endpoint.postman', slugify(path), url, (error, result) => {
+      if (error) return;
+      download(result.toString(), slugify(path), 'application/json');
+    })
+  }
+})
 
 Template.triggerEditorEndpointEventNewContent.onRendered(function() {
   const trigger = this.data.flow ? this.data.flow.trigger : {}
   if (!(trigger.config || {}).endpoint) {
     $('[name="trigger.config.endpoint"]').val(uuidv4())
-  }
-})
-
-Template.triggerEditorEndpointEventNewContent.events({
-  'keyup #s-endpoint-url': (event, tpl) => {
-    console.log({event, tpl})
   }
 })
--- imports/services/endpoint/server/methods.js
+++ imports/services/endpoint/server/methods.js
@@ -1,6 +1,141 @@
+const url = require('url')
+const uuidv4 = require('uuid/v4')
+
+const buildTemplate = (name, _url) => {
+  const urlParsed = url.parse(_url)
+  let protocol = urlParsed.protocol.replace(':', '')
+  let host = urlParsed.hostname
+  let port = urlParsed.port || protocol === 'https' ? '443' : '80'
+  let pathParts = urlParsed.path.split('/').filter(Boolean)
+  let path = pathParts.length ? `["${pathParts.join('","')}"]` : '[]'
+
+  return `{
+    "info": {
+      "_postman_id": "${uuidv4()}",
+      "name": "${name}",
+      "schema": "https://schema.getpostman.com/json/collection/v2.1.0/collection.json"
+    },
+    "item": [
+      {
+        "name": "GET",
+        "request": {
+          "method": "GET",
+          "header": [],
+          "url": {
+            "raw": "${_url}",
+            "protocol": "${protocol}",
+            "host": [
+              "${host}"
+            ],
+            "port": "${port}",
+            "path": ${path}
+          }
+        },
+        "response": []
+      },
+      {
+        "name": "POST",
+        "request": {
+          "method": "POST",
+          "header": [],
+          "body": {
+            "mode": "raw",
+            "raw": "{\\"hello\\": \\"world\\"}",
+            "options": {
+              "raw": {
+                "language": "json"
+              }
+            }
+          },
+          "url": {
+            "raw": "${_url}",
+            "protocol": "${protocol}",
+            "host": [
+              "${host}"
+            ],
+            "port": "${port}",
+            "path": ${path}
+          }
+        },
+        "response": []
+      },
+      {
+        "name": "PUT",
+        "request": {
+          "method": "PUT",
+          "header": [],
+          "body": {
+            "mode": "raw",
+            "raw": "{\\"hello\\": \\"world\\"}",
+            "options": {
+              "raw": {
+                "language": "json"
+              }
+            }
+          },
+          "url": {
+            "raw": "${_url}",
+            "protocol": "${protocol}",
+            "host": [
+              "${host}"
+            ],
+            "port": "${port}",
+            "path": ${path}
+          }
+        },
+        "response": []
+      },
+      {
+        "name": "PATCH",
+        "request": {
+          "method": "PATCH",
+          "header": [],
+          "body": {
+            "mode": "raw",
+            "raw": "{\\"hello\\": \\"world\\"}",
+            "options": {
+              "raw": {
+                "language": "json"
+              }
+            }
+          },
+          "url": {
+            "raw": "${_url}",
+            "protocol": "${protocol}",
+            "host": [
+              "${host}"
+            ],
+            "port": "${port}",
+            "path": ${path}
+          }
+        },
+        "response": []
+      },
+      {
+        "name": "DELETE",
+        "request": {
+          "method": "DELETE",
+          "header": [],
+          "url": {
+            "raw": "${_url}",
+            "protocol": "${protocol}",
+            "host": [
+              "${host}"
+            ],
+            "port": "${port}",
+            "path": ${path}
+          }
+        },
+        "response": []
+      }
+    ],
+    "protocolProfileBehavior": {}
+  }`
+}
+
 Meteor.methods({
-  's-endpoint.getTemplate': name => {
+  's-endpoint.postman': (name, _url) => {
     if (!Meteor.userId()) throw new Meteor.Error('no-auth')
-    return Assets.getText(`requests/${name}.html`)
+    return buildTemplate(name, _url)
   }
 })
