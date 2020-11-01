80c99f0028074d39036657aa71a4c32094657811
Jose Constela <jose@joseconstela.com>
2020-11-01 12:04:26 +0100

    Linting errors

--- private/prefill/nodesfc/aws-sns-message.js
+++ private/prefill/nodesfc/aws-sns-message.js
@@ -1,25 +1,25 @@
 // Load the AWS SDK for Node.js
-var AWS = require('aws-sdk')
+var AWS = require('aws-sdk');
 
-AWS.config.accessKeyId = process.env.accessKeyId
-AWS.config.secretAccessKey = process.env.secretAccessKey
-AWS.config.region = 'us-east-1'
+AWS.config.accessKeyId = process.env.accessKeyId;
+AWS.config.secretAccessKey = process.env.secretAccessKey;
+AWS.config.region = 'us-east-1';
 
 // Create publish parameters
 var params = {
   Message: 'MESSAGE_TEXT', /* required */
   TopicArn: 'TOPIC_ARN'
-}
+};
 
 // Create promise and SNS service object
-var publishTextPromise = new AWS.SNS({apiVersion: '2010-03-31'}).publish(params).promise()
+var publishTextPromise = new AWS.SNS({apiVersion: '2010-03-31'}).publish(params).promise();
 
 // Handle promise's fulfilled/rejected states
 publishTextPromise.then(
   (data) => {
-    console.log(`Message ${params.Message} send sent to the topic ${params.TopicArn}`)
-    console.log(`MessageID is ${data.MessageId}`)
+    console.log(`Message ${params.Message} send sent to the topic ${params.TopicArn}`);
+    console.log(`MessageID is ${data.MessageId}`);
   })
   .catch(ex => {
-    console.error(ex, ex.stack)
-  })
+    console.error(ex, ex.stack);
+  });
